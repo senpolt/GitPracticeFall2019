b294484d1821dedb6cf08a3fd6c0549d828e8852
senpolt <senpolt@gmail.com>
2020-03-12 14:30:56 -0500

    gitProject

---
+++ src/exp/LoopPractice.java
@@ -0,0 +1,10 @@
+package exp;
+
+public class LoopPractice {
+    public static void main(String[] args) {
+        for (int i = 0; i <10 ; i++) {
+            System.out.println("i = " + i);
+
+        }
+    }
+}
---
+++ src/exp/StringPractice.java
@@ -0,0 +1,8 @@
+package exp;
+
+public class StringPractice {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Java");
+    }
+}
